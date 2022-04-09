f46f816af75a87fe76b2319b1106de25d14b3dad
Adam Spiers <blockchain@adamspiers.org>
2022-04-09 20:20:10 +0100

    Switch to TypeScript

--- source/options.ts
+++ source/options.ts
@@ -1,7 +1,7 @@
 // Don't forget to import this wherever you use it
 import browser from 'webextension-polyfill';
 
-import optionsStorage from './options-storage.js';
+import optionsStorage from './options-storage';
 
 optionsStorage.syncForm('#options-form');
 
