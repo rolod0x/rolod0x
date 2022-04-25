7789003ba1d3621ff501f85f3cc561e4f80eacca
Adam Spiers <blockchain@adamspiers.org>
2022-04-26 00:45:56 +0200

    match uppercase addresses with checksumming

--- source/parser.ts
+++ source/parser.ts
@@ -1,7 +1,33 @@
+import {utils} from 'ethers';
 import {LabelMap} from './types.ts';
 
 function tenderlyAddress(address: string): string {
     return address.slice(0, 10) + '...' + address.slice(-4);
+}
+
+function addLabel(labelMap: LabelMap, i: number, line: string, address: string, label: string, comment?: string) {
+    const addresses = [address];
+    let canonical;
+    try {
+        canonical = utils.getAddress(address);
+    }
+    catch (err: any) {
+        if (err.message.match(/bad address checksum/)) {
+            throw new Error(
+                `Bad address checksum on line ${i + 1}:\n` + line);
+        }
+        throw err;
+    }
+    if (address != canonical) {
+        addresses.push(canonical);
+    }
+
+    for (const a of addresses) {
+        labelMap[a] = labelMap[tenderlyAddress(a)] = {
+            label,
+            comment,
+        };
+    }
 }
 
 export function parseLabels(labels: string): LabelMap {
@@ -18,10 +44,7 @@ export function parseLabels(labels: string): LabelMap {
         if (m) {
             const [_all, address, label, comment] = m;
             if (address && label) {
-                labelMap[address] = labelMap[tenderlyAddress(address)] = {
-                    label,
-                    comment,
-                };
+                addLabel(labelMap, i, line, address, label, comment);
                 continue;
             }
 
