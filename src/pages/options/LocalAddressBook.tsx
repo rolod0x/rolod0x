a81a82c0d8928851afde54656170f61f364581cd
Adam Spiers <blockchain@adamspiers.org>
2023-12-30 17:15:13 +0000

    chore: use useCallback() in LocalAddressBook to memoize helper functions

--- src/pages/options/LocalAddressBook.tsx
+++ src/pages/options/LocalAddressBook.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Box } from '@mui/system';
 import Typography from '@mui/material/Typography';
 
@@ -9,37 +9,44 @@ export default function LocalAddressBook() {
   const [labels, setLabels] = useState('');
   const [error, setError] = useState<string | null>();
 
-  const validate = (labels: string): void => {
-    if (!labels) return;
+  const validate = useCallback(
+    (labels: string): void => {
+      if (!labels) return;
 
-    try {
-      console.log(`Parsing: ${labels.slice(0, 30)}...`);
-      const [linesParsed, _labelMap] = parseLabels(labels);
-      console.log(`Parsed ${linesParsed} lines`);
-      setError(null);
-    } catch (err: unknown) {
-      console.error(err);
-      if (err instanceof Error) {
-        setError(err.message);
+      try {
+        console.log(`Parsing: ${labels.slice(0, 30)}...`);
+        const [linesParsed, _labelMap] = parseLabels(labels);
+        console.log(`Parsed ${linesParsed} lines`);
+        setError(null);
+      } catch (err: unknown) {
+        console.error(err);
+        if (err instanceof Error) {
+          setError(err.message);
+        }
       }
-    }
-  };
+    },
+    [setError],
+  );
 
-  const handleLabelsChange = async event => {
-    setLabels(event.target.value);
-    await optionsStorage.set({ labels: event.target.value });
-    validate(event.target.value);
-  };
+  const handleLabelsChange = useCallback(
+    async event => {
+      setLabels(event.target.value);
+      await optionsStorage.set({ labels: event.target.value });
+      validate(event.target.value);
+    },
+    [setLabels, validate],
+  );
+
+  const getOptions = useCallback(async () => {
+    const options = await optionsStorage.getAll();
+    console.log('Hydrated options from storage');
+    setLabels(options.labels);
+    validate(labels);
+  }, [labels, setLabels, validate]);
 
   useEffect(() => {
-    async function getOptions() {
-      const options = await optionsStorage.getAll();
-      console.log('Hydrated options from storage');
-      setLabels(options.labels);
-      validate(labels);
-    }
     getOptions();
-  });
+  }, [getOptions]);
 
   return (
     <form className="detail-view-container">
