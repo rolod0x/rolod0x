f60febba591a28e2939c70595128c4c1e6f2c994
Adam Spiers <blockchain@adamspiers.org>
2024-01-07 17:49:32 +0000

    test(parser): add tests for zeroPaddedAddress()

--- src/shared/parser.test.ts
+++ src/shared/parser.test.ts
@@ -1,4 +1,4 @@
-import { abbreviatedAddresses } from './parser';
+import { abbreviatedAddresses, zeroPaddedAddress } from './parser';
 
 describe('parser', () => {
   describe('abbreviatedAddresses', () => {
@@ -8,6 +8,15 @@ describe('parser', () => {
         '0x6B17...1d0F',
         '0x6B1754...95271d0F',
         '0x6B1...71d0F',
+      ]);
+    });
+  });
+
+  describe('zeroPaddedAddress', () => {
+    test('to output an array of two zero-padded addresses', () => {
+      expect(zeroPaddedAddress('0x6B175474E89094C44Da98b954EedeAC495271d0F')).toEqual([
+        '0000000000000000000000006B175474E89094C44Da98b954EedeAC495271d0F',
+        '0x0000000000000000000000006B175474E89094C44Da98b954EedeAC495271d0F',
       ]);
     });
   });
