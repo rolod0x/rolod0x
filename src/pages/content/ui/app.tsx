9a7d9b9302fe8e3572aebfc3d5e61365b120fba7
Adam Spiers <blockchain@adamspiers.org>
2024-01-20 19:21:25 +0000

    fix(content): remove old injected HTML
    This was never needed; just a remnant from the extension template.

--- src/pages/content/ui/app.tsx
+++ src/pages/content/ui/app.tsx
@@ -1,9 +1,15 @@
 import { useEffect } from 'react';
 
+// Not used, just keeping this here in case we ever need to resurrect it.
+
 export default function App() {
   useEffect(() => {
     console.log('content view loaded');
   }, []);
 
-  return <div className="">content view</div>;
+  return (
+    <div className="">
+      Content view should not be visible; if you are seeing this, please report as a bug!
+    </div>
+  );
 }
