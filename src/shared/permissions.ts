c5d6e422f4ec6f60506db7c8c84a2beed3fb77e9
Adam Spiers <blockchain@adamspiers.org>
2024-01-21 01:40:28 +0000

    chore(permissions): improve debugging

--- src/shared/permissions.ts
+++ src/shared/permissions.ts
@@ -1,25 +1,25 @@
-import { isContentScriptRegistered } from 'webext-dynamic-content-scripts/utils.js';
+// import { isContentScriptRegistered } from 'webext-dynamic-content-scripts/utils.js';
 import { queryAdditionalPermissions, normalizeManifestPermissions } from 'webext-permissions';
 
 export async function checkPermissions(reason: string): Promise<void> {
   console.log('reason: ', reason);
   const manifest = chrome.runtime.getManifest();
   console.log('manifest: ', manifest);
-  const matches = [
-    ...manifest.content_scripts[0].matches,
-    'https://developer.chrome.com/foo',
-    'https://etherscan.io/foo',
-  ];
-  //console.log('manifest.content_scripts: ', matches);
-  for (const match of matches) {
-    const mode = await isContentScriptRegistered(match);
-    console.log(`${match}: ${mode}`);
-  }
+  // const matches = [
+  //   ...manifest.content_scripts[0].matches,
+  //   'https://developer.chrome.com/foo',
+  //   'https://etherscan.io/foo',
+  // ];
+  // //console.log('manifest.content_scripts: ', matches);
+  // for (const match of matches) {
+  //   const mode = await isContentScriptRegistered(match);
+  //   console.log(`${match}: ${mode}`);
+  // }
 
   // Returns mix of manifest and new, as explained in
   // https://github.com/fregante/webext-permissions
-  // const perms = await chrome.permissions.getAll();
-  // console.log('perms: ', perms);
+  const perms = await chrome.permissions.getAll();
+  console.log('perms.origins: ', perms.origins);
 
   const newPermissions = await queryAdditionalPermissions();
   console.log('newPermissions.origins: ', newPermissions.origins);
