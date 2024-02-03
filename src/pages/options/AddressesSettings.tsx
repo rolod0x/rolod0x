e333640bcf43289ef85bc18d43eb1802b599bd75
Adam Spiers <blockchain@adamspiers.org>
2024-02-03 15:55:04 +0000

    fix(options): only use outlined variant for alerts in dark mode

--- src/pages/options/AddressesSettings.tsx
+++ src/pages/options/AddressesSettings.tsx
@@ -1,3 +1,4 @@
+import { useContext } from 'react';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
@@ -6,12 +7,16 @@ import Box from '@mui/material/Box';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Typography from '@mui/material/Typography';
 
+import { ThemeNameContext } from '../../components/Rolod0xThemeProvider';
+
 import LocalAddressBook from './LocalAddressBook';
 
 export default function AddressesSettings() {
+  const { themeName } = useContext(ThemeNameContext);
+
   return (
     <Box>
-      <Alert severity="warning" variant="outlined">
+      <Alert severity="warning" variant={themeName === 'dark' ? 'outlined' : 'standard'}>
         After changing entries in the address book, you may have to reload pages for the changes to
         take effect.
       </Alert>
