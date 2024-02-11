a85218263cc89c5bd4aba9553b559e03efeb80cd
Adam Spiers <blockchain@adamspiers.org>
2024-02-11 20:32:01 +0000

    fix: remove unnecessary containers
    <Dialog> already uses <Paper> internally

--- src/pages/lookup/ui/Lookup.tsx
+++ src/pages/lookup/ui/Lookup.tsx
@@ -1,6 +1,3 @@
-import Container from '@mui/material/Container';
-import Paper from '@mui/material/Paper';
-
 import Rolod0xThemeProvider from '../../../components/Rolod0xThemeProvider';
 
 import ActionDialog from './ActionDialog';
@@ -8,11 +5,7 @@ import ActionDialog from './ActionDialog';
 export default function Lookup() {
   return (
     <Rolod0xThemeProvider>
-      <Container style={{ position: 'fixed', top: 200, minWidth: 500, maxWidth: 1000 }}>
-        <Paper elevation={2}>
-          <ActionDialog />
-        </Paper>
-      </Container>
+      <ActionDialog />
     </Rolod0xThemeProvider>
   );
 }
