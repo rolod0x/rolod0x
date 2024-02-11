e3b518c7b719e7da9eb7593408df0c6514ef88e7
Adam Spiers <blockchain@adamspiers.org>
2024-02-11 21:36:41 +0000

    chore(Donate): prefer ETH to BTC

--- src/pages/options/Donate.tsx
+++ src/pages/options/Donate.tsx
@@ -18,16 +18,16 @@ export default function Donate() {
       </Typography>
       <ul>
         <li>
-          Send <strong>Bitcoin</strong> to:{' '}
-          <StyledCode>bc1quuspvrjepx63k5hpydwqkf6nmtt9eqm86y8w8a</StyledCode>
-        </li>
-        <li>
           Send <strong>ETH</strong> or tokens on any Ethereum network to:{' '}
           <StyledCode>rolod0x.eth</StyledCode>
           <br />
           (N.B. that's a zero before the <StyledCode>x</StyledCode>, not an uppercase{' '}
           <StyledCode>O</StyledCode> &mdash; the address should resolve to{' '}
           <StyledCode>0x06357397d8078C19195f4555db7A407b1b1f5FB3</StyledCode>.)
+        </li>
+        <li>
+          Send <strong>Bitcoin</strong> to:{' '}
+          <StyledCode>bc1quuspvrjepx63k5hpydwqkf6nmtt9eqm86y8w8a</StyledCode>
         </li>
       </ul>
       <Typography paragraph>
