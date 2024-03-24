e437bfd5897cae4e922f3d91d64dfdd6755236ee
Adam Spiers <blockchain@adamspiers.org>
2024-03-24 00:02:28 +0000

    refactor(settings): extract NavBar to separate component

---
+++ src/pages/options/NavBar.tsx
@@ -0,0 +1,43 @@
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+
+import OptionsDrawer from './OptionsDrawer';
+
+interface Props {
+  drawerWidth: number;
+  handleDrawerToggle: () => void;
+  mobileOpen: boolean;
+}
+
+export default function NavBar({ drawerWidth, handleDrawerToggle, mobileOpen }: Props) {
+  return (
+    <Box
+      component="nav"
+      sx={{ width: { md: drawerWidth }, flexShrink: { md: 0 } }}
+      aria-label="rolod0x options">
+      {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+      <Drawer
+        variant="temporary"
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{
+          keepMounted: true, // Better open performance on mobile.
+        }}
+        sx={{
+          display: { xs: 'block', md: 'none' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+        }}>
+        <OptionsDrawer />
+      </Drawer>
+      <Drawer
+        variant="permanent"
+        sx={{
+          display: { xs: 'none', md: 'block' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+        }}
+        open>
+        <OptionsDrawer />
+      </Drawer>
+    </Box>
+  );
+}
--- src/pages/options/ResponsiveDrawer.tsx
+++ src/pages/options/ResponsiveDrawer.tsx
@@ -1,13 +1,12 @@
 import { useCallback, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
 import Toolbar from '@mui/material/Toolbar';
 
 import './ResponsiveDrawer.css';
 
+import NavBar from './NavBar';
 import SettingsAppBar from './SettingsAppBar';
-import OptionsDrawer from './OptionsDrawer';
 
 const drawerWidth = 200;
 
@@ -24,34 +23,9 @@ export default function ResponsiveDrawer() {
   return (
     <Box display="flex">
       <SettingsAppBar {...{ drawerWidth, handleDrawerToggle }} />
-      <Box
-        component="nav"
-        sx={{ width: { md: drawerWidth }, flexShrink: { md: 0 } }}
-        aria-label="rolod0x options">
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-        <Drawer
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block', md: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}>
-          <OptionsDrawer />
-        </Drawer>
-        <Drawer
-          variant="permanent"
-          sx={{
-            display: { xs: 'none', md: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-          open>
-          <OptionsDrawer />
-        </Drawer>
-      </Box>
+      <NavBar {...{ drawerWidth, handleDrawerToggle, mobileOpen }} />
+
+      {/* main content */}
       <Box
         component="main"
         sx={{ flexGrow: 1, p: 3, width: { md: `calc(100% - ${drawerWidth}px)` } }}>
