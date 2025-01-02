d11ebe98103f9f7ec47db0d8f0eb6e219aa47da0
Adam Spiers <blockchain@adamspiers.org>
2025-01-02 00:23:11 +0000

    refactor(options): move address book storage handling into parent
    This prepares for switching to a multi-section address book.

--- src/pages/options/AddressesSettings.tsx
+++ src/pages/options/AddressesSettings.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
@@ -6,9 +7,27 @@ import Box from '@mui/material/Box';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Typography from '@mui/material/Typography';
 
+import { useAddressBook } from '@src/shared/hooks/useAddressBook';
+
 import LocalAddressBook from './LocalAddressBook';
 
 export default function AddressesSettings() {
+  const {
+    labels,
+    error,
+    currentLabelsHash,
+    savedLabelsHash,
+    setLabels,
+    setCurrentLabelsHash,
+    handleSave,
+    getOptions,
+    validate,
+  } = useAddressBook();
+
+  useEffect(() => {
+    getOptions();
+  }, [getOptions]);
+
   return (
     <Box>
       <Alert severity="warning" variant="outlined" sx={{ borderWidth: 3 }}>
@@ -25,7 +44,17 @@ export default function AddressesSettings() {
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <LocalAddressBook />
+          <LocalAddressBook
+            labels={labels}
+            error={error}
+            currentLabelsHash={currentLabelsHash}
+            savedLabelsHash={savedLabelsHash}
+            onLabelsChange={setLabels}
+            onCurrentLabelsHashChange={setCurrentLabelsHash}
+            onSave={handleSave}
+            onGetOptions={getOptions}
+            validate={validate}
+          />
         </AccordionDetails>
       </Accordion>
     </Box>
---
+++ src/shared/hooks/useAddressBook.ts
@@ -0,0 +1,65 @@
+import { useCallback, useState } from 'react';
+import * as murmurhash from 'murmurhash';
+
+import { Parser, ParseError } from '@src/shared/parser';
+import { optionsStorage } from '@src/shared/options-storage';
+
+export function useAddressBook() {
+  const [labels, setLabels] = useState('');
+  const [currentLabelsHash, setCurrentLabelsHash] = useState(murmurhash.v3(''));
+  const [savedLabelsHash, setSavedLabelsHash] = useState(murmurhash.v3(''));
+  const [error, setError] = useState<string | null>();
+
+  const validate = useCallback(
+    (labels: string): void => {
+      if (!labels) return;
+
+      try {
+        console.log(`Parsing: ${labels.slice(0, 30)}...`);
+        const parser = new Parser(labels);
+        console.log(`Parsed ${parser.parsedEntries.length} entries`);
+        setError(null);
+      } catch (err: unknown) {
+        if (err instanceof ParseError) {
+          console.log(err.message);
+          setError(`Please check that line ${err.lineNumber} is in the correct format.`);
+        } else if (err instanceof Error) {
+          console.error(err.message);
+          setError(err.message);
+        } else {
+          console.error(err);
+        }
+      }
+    },
+    [setError],
+  );
+
+  const handleSave = useCallback(async () => {
+    await optionsStorage.set({ labels });
+    const hash = murmurhash.v3(labels);
+    setSavedLabelsHash(hash);
+    validate(labels);
+  }, [labels, validate]);
+
+  const getOptions = useCallback(async () => {
+    const options = await optionsStorage.getAll();
+    setLabels(options.labels);
+    const hash = murmurhash.v3(options.labels);
+    console.log(`Hydrated options from storage (hash ${hash})`);
+    setCurrentLabelsHash(hash);
+    setSavedLabelsHash(hash);
+    validate(options.labels);
+  }, [setLabels, validate]);
+
+  return {
+    labels,
+    error,
+    currentLabelsHash,
+    savedLabelsHash,
+    setLabels,
+    setCurrentLabelsHash,
+    handleSave,
+    getOptions,
+    validate,
+  };
+}
