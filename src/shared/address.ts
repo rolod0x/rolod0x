a71dee0425ce2831a5a86bf40994ca6ed8c831bf
Adam Spiers <blockchain@adamspiers.org>
2025-01-03 19:10:52 +0000

    test(address): add tests for getCanonicalAddress()
    Also fix a corner case where 0x is omitted.

--- src/shared/address.ts
+++ src/shared/address.ts
@@ -2,6 +2,9 @@ import { getAddress } from 'ethers';
 
 export function getCanonicalAddress(address: string): string | null {
   try {
+    if (typeof address !== 'string' || !address.startsWith('0x')) {
+      return null;
+    }
     return getAddress(address);
   } catch (err: unknown) {
     return null;
