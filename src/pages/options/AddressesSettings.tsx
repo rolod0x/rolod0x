38b93b14593056b763fcac68fd8c09c50f706b7e
Adam Spiers <blockchain@adamspiers.org>
2025-01-04 01:06:05 +0000

    feat(AddressesSettings): add hints about right-click and searching

--- src/pages/options/AddressesSettings.tsx
+++ src/pages/options/AddressesSettings.tsx
@@ -2,6 +2,8 @@ import { useCallback, useEffect, useState } from 'react';
 import Alert from '@mui/material/Alert';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
 import { v4 as uuidv4 } from 'uuid';
 import Joyride from 'react-joyride';
 
@@ -85,6 +87,34 @@ export default function AddressesSettings() {
           },
         }}
       />
+      <Alert
+        severity="info"
+        sx={{
+          borderWidth: 3,
+          mb: 2,
+          '& .MuiAlert-icon': {
+            marginTop: '4px',
+          },
+        }}>
+        <Typography variant="h6" sx={{ mt: 0 }}>
+          Did you know?
+        </Typography>
+        You can also label addresses simply by{' '}
+        <Link
+          href="https://rolod0x.io/docs/user-manual.html#adding"
+          target="_noblank"
+          rel="noreferrer noopener">
+          right-clicking on them
+        </Link>
+        , and you can{' '}
+        <Link
+          href="https://rolod0x.io/docs/user-manual.html#lookup"
+          target="_noblank"
+          rel="noreferrer noopener">
+          quickly look up addresses via a hotkey or by clicking the extension icon
+        </Link>
+        .
+      </Alert>
       <Alert severity="warning" variant="outlined" sx={{ borderWidth: 3 }}>
         After changing entries in the address book, you may have to reload pages for the changes to
         take effect.
