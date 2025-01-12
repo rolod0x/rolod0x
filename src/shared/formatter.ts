eadc669aeeea541b6cb78069ae14abf9115e273c
Adam Spiers <blockchain@adamspiers.org>
2025-01-12 16:25:37 +0000

    feat(formatter): support extraction of inner digits
    This allows formatting which includes spaces or other characters,
    like Kraken does, e.g.
    
        0xe3 D823 ... D62e F109

--- src/shared/formatter.ts
+++ src/shared/formatter.ts
@@ -12,10 +12,15 @@ export class Formatter {
   }
 
   format(label: Label, address: Address): string {
+    const trimmed = address.replace(/^0x/, '');
     return this.formatString
       .replace('%n', label)
       .replace('%a', address)
-      .replace(/%(\d+)l/, (_match, digits) => address.replace(/^0x/, '').slice(0, Number(digits)))
-      .replace(/%(\d+)r/, (_match, digits) => address.replace(/^0x/, '').slice(-Number(digits)));
+      .replace(/%(\d+)l/, (_match, digits) => trimmed.slice(0, Number(digits)))
+      .replace(/%(\d+)r/, (_match, digits) => trimmed.slice(-Number(digits)))
+      .replaceAll(/%(-?\d+)i(\d+)/g, (_match, offset, length) => {
+        const startAt = Number(offset) < 0 ? trimmed.length + Number(offset) : Number(offset);
+        return trimmed.slice(startAt, startAt + Number(length));
+      });
   }
 }
