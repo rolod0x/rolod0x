03f2a4b9081c260a1925427c1db80040f992a4e1
Michalis Kargakis <kargakis@protonmail.com>
2025-01-20 20:56:55 +0100

    test: fix test description

--- src/shared/transformers.test.ts
+++ src/shared/transformers.test.ts
@@ -1,7 +1,7 @@
 import { zeroPaddedAddress } from './transformers';
 
 describe('zeroPaddedAddress', () => {
-  it('outputs an array of four zero-padded addresses', () => {
+  it('outputs an array of two zero-padded addresses', () => {
     expect(zeroPaddedAddress('0x6B175474E89094C44Da98b954EedeAC495271d0F')).toEqual([
       '0000000000000000000000006B175474E89094C44Da98b954EedeAC495271d0F',
       '0x0000000000000000000000006B175474E89094C44Da98b954EedeAC495271d0F',
