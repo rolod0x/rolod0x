e8fd6bb2f588b1c5267b9b207d36a17a359ce503
Michalis Kargakis <kargakis@protonmail.com>
2025-01-20 15:05:20 +0100

    refactor: move single line parsing into its own function

--- src/shared/parser.ts
+++ src/shared/parser.ts
@@ -30,43 +30,46 @@ export class Parser {
   }
 
   parseMultiline(unparsedLabels: string): ParsedEntries {
-    const labelLineRe = /^s*(0x[\da-f]{40})\s+(.+?)(?:\s+\/\/\s*(.*?)\s*)?$/i;
     const lines = unparsedLabels.split('\n');
-    lines.forEach((line, i) => {
-      if (/^\s*(\/\/|$)/.test(line)) {
-        // Comment or blank line; ignore
+    lines.forEach((line, lineIndex) => {
+      this.parseLine(line, lineIndex);
+    });
+    return this.parsedEntries;
+  }
+
+  private parseLine(line: string, lineIndex: number): void {
+    if (/^\s*(\/\/|$)/.test(line)) {
+      // Comment or blank line; ignore
+      return;
+    }
+
+    const labelLineRe = /^s*(0x[\da-f]{40})\s+(.+?)(?:\s+\/\/\s*(.*?)\s*)?$/i;
+    const m = labelLineRe.exec(line);
+    if (m) {
+      const [_all, address, label, comment] = m;
+      if (address && label) {
+        let canonical: string;
+        try {
+          canonical = getAddress(address);
+        } catch (err: unknown) {
+          if (err instanceof Error && err.message.match(/bad address checksum/)) {
+            throw new ParseError(lineIndex + 1, `Bad address checksum`);
+          }
+          throw err;
+        }
+        this.addEntry(canonical, label, comment);
         return;
       }
 
-      const m = labelLineRe.exec(line);
-      if (m) {
-        const [_all, address, label, comment] = m;
-        if (address && label) {
-          let canonical: string;
-          try {
-            canonical = getAddress(address);
-          } catch (err: unknown) {
-            if (err instanceof Error && err.message.match(/bad address checksum/)) {
-              throw new ParseError(i + 1, `Bad address checksum`);
-            }
-            throw err;
-          }
-          this.addEntry(canonical, label, comment);
-          return;
-        }
+      throw new Error(
+        `BUG: parsing issue with line ${lineIndex + 1}; ` +
+          `address=${address ?? 'undefined'}, ` +
+          `label=${label ?? 'undefined'}:\n` +
+          line,
+      );
+    }
 
-        throw new Error(
-          `BUG: parsing issue with line ${i + 1}; ` +
-            `address=${address ?? 'undefined'}, ` +
-            `label=${label ?? 'undefined'}:\n` +
-            line,
-        );
-      }
-
-      throw new ParseError(i + 1);
-    });
-
-    return this.parsedEntries;
+    throw new ParseError(lineIndex + 1);
   }
 
   addEntry(address: Address, label: Label, comment?: Comment): void {
