4f2c5b43d93d9cc6aa9b4f399ccca1bfa4241ee6
Michalis Kargakis <kargakis@protonmail.com>
2025-01-20 19:23:15 +0100

    chore: restrict zeroPaddedAddress transformation to EVM

--- src/shared/transformers.test.ts
+++ src/shared/transformers.test.ts
@@ -7,4 +7,8 @@ describe('zeroPaddedAddress', () => {
       '0x0000000000000000000000006B175474E89094C44Da98b954EedeAC495271d0F',
     ]);
   });
+
+  it('outputs an empty array for an invalid address', () => {
+    expect(zeroPaddedAddress('DYw8jCTfwHNRJhhmFcbXvVDTqWMEVFBX6ZKUmG5CNSKK')).toEqual([]);
+  });
 });
--- src/shared/transformers.ts
+++ src/shared/transformers.ts
@@ -1,6 +1,11 @@
+import { getCanonicalAddress } from './address';
+
 // Spot addresses embedded into EVM 256 bit words.  This is typically
 // seen in event data.
 export function zeroPaddedAddress(address: string): string[] {
+  if (!getCanonicalAddress(address)) {
+    return [];
+  }
   // Addresses are 20 bytes, so to pad to 32 bytes we need an extra
   // 12 bytes of zero padding (24 nibbles).
   const padded = '00'.repeat(12) + address.slice(2);
